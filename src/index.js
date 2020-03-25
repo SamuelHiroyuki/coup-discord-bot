0c8bc7497ffd412d66da3943f436b8368b0ee685
SamuelHiroyuki <samduasvezesmortal@gmail.com>
2020-03-25 19:49:34 -0300

    Added Start command and Guilds

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ require("./database");
 
 const Discord = require("discord.js");
 const commands = require("./commands");
+const Guild = require("./models/Guild");
 
 const client = new Discord.Client();
 
@@ -12,10 +13,21 @@ client.on("ready", () => {
 	client.user.setActivity(" Coup");
 });
 
+client.on("guildCreate", guild => {
+	Guild.create({
+		discord_id: guild.id,
+		discord_name: guild.name
+	});
+});
+
+client.on("guildDelete", async guild => {
+	await Guild.findOneAndRemove({ discord_id: guild.id });
+});
+
 client.on("message", receivedMessage => {
 	const { author, content } = receivedMessage;
 
-	if (author === client.user) return;
+	if (author.bot) return;
 
 	if (content.startsWith("[") && content.endsWith("]")) {
 		const command = commands[content];
