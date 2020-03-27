09f0bad76c0255e5d095b6311d8b86c4608e6409
SamuelHiroyuki <samduasvezesmortal@gmail.com>
2020-03-27 14:11:45 -0300

    Fix Emojis command

--- src/utils/calcEmoji.js
+++ src/utils/calcEmoji.js
@@ -1,7 +1,8 @@
+const _ = require("lodash");
 const influences = require("../assets/json/influences.json");
 
 module.exports = emojis => {
 	const values = Object.values(influences);
 	const serverEmojis = values.filter(v => emojis & v);
-	return values.filter(v => !serverEmojis.includes(v));
+	return _.difference(values, serverEmojis);
 };
